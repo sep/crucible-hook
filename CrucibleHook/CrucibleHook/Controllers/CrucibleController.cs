7202708e5da793c2d7a4259adabb6270f544243c
Aaron Alexander <ajalexander@sep.com>
2014-07-07 09:35:25 -0400

    Adding WebAPI project

---
+++ CrucibleHook/CrucibleHook/Controllers/CrucibleController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Web.Configuration;
+using System.Web.Http;
+
+namespace CrucibleHook.Controllers
+{
+    public class CrucibleController : ApiController
+    {
+        /// <summary>
+        /// test
+        /// </summary>
+        /// <remarks>
+        /// GET api/crucible/jenkins-test
+        /// </remarks>
+        /// <param name="id">The name of the Crucible repository to notify</param>
+        [HttpGet]
+        public void Notify(string id)
+        {
+            var client = new WebClient();
+
+            SetupHeaders(client);
+
+            var url = BuildUrl(id);
+
+            client.UploadValues(url, "POST", new NameValueCollection());
+        }
+
+        /// <summary>
+        /// Builds the URL.
+        /// </summary>
+        /// <param name="repositoryName">Name of the repository in Crucible.</param>
+        /// <returns>A formatted URL.</returns>
+        private static string BuildUrl(string repositoryName)
+        {
+            var baseUrl = WebConfigurationManager.AppSettings["CrucibleBaseUrl"];
+            var notifyUrl = String.Format(WebConfigurationManager.AppSettings["CrucibleNotifyUrlFormat"], repositoryName);
+            var url = baseUrl + notifyUrl;
+            return url;
+        }
+
+        /// <summary>
+        /// Setups the headers for the request.
+        /// </summary>
+        /// <param name="client">The client.</param>
+        private static void SetupHeaders(WebClient client)
+        {
+            client.Headers[HttpRequestHeader.ContentType] = "application/json";
+            client.Headers["X-Api-Key"] = WebConfigurationManager.AppSettings["ApiKey"];
+        }
+    }
+}
---
+++ CrucibleHook/CrucibleHook/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CrucibleHook")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SEP")]
+[assembly: AssemblyProduct("CrucibleHook")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("89da604e-5d7a-46b4-883b-70df47664eb7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
